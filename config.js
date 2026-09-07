0e5fd0e85ea78cee859cc21b6357607ec56285be
uqpropoc-git <uqpropoc@gmail.com>
2026-09-07 15:38:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbz2eU1Ymca7VTSvVGHEYhG3TJp7W74no-2NFMe2c1SmE6lSC068N8x-bPuRw-VJF6UvIw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzWIeraUi78kTUzdBoDUcO4bC1dL6Xjr4pbkJgmt5HfFAp6L9hkES9deXmG78Ei_fZD/exec";
